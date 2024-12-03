ea291460912f10d500d4e8482033f947f2896b87
Felipe Torres <felipe.torres@lis-lab.fr>
2024-12-03 13:23:38 +0100

    Data files with randomization scripts brought back

---
+++ data/sorting_50k.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pdb
+import random
+import numpy as np
+from more_itertools import locate
+
+np.random.seed(44)
+permutation = np.random.permutation(50)
+file_reading = 'revisited_imagenet_2012_val.csv'
+with open(file_reading, 'r') as data:
+    ctnts = data.readlines()
+
+# First for loop. Getting all the classes in an ordered manner.
+classes = []
+for line in ctnts:
+    name, class_id = line.strip().split(',')
+    classes.append(class_id)
+
+writing_list = []
+for class_id in range(1000):
+    class_elems = list(locate(classes, lambda x: x == str(class_id)))
+    retrieving = [class_elems[int(permutation[x])] for x in range(50)]
+    from_orign = [ctnts[idx] for idx in retrieving]
+    writing_list+=from_orign
+
+#np.random.shuffle(writing_list)
+with open('50k_sorted.csv', 'w') as data:
+    for line in writing_list:
+        data.write(line)
